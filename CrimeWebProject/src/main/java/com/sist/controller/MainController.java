dadfc237cd5f5f0f63142f57cd5c7c255337eef6
sist <sist@ns6>
2018-01-26 17:44:19 +0900

    cs

--- CrimeWebProject/src/main/java/com/sist/controller/MainController.java
+++ CrimeWebProject/src/main/java/com/sist/controller/MainController.java
@@ -1,13 +1,33 @@
 package com.sist.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sist.freeboard.BoardVO;
+import com.sist.main.Item;
+import com.sist.main.MainDAO;
 
 
 @Controller
 public class MainController {
+	@Autowired
+	private MainDAO dao;
+	
 	@RequestMapping("main.do")
-	public String crimerate(){
+	public String crimerate(Model model){
+		List<BoardVO> mBoard = new ArrayList<BoardVO>();
+		List<Item> mNews = new ArrayList<Item>();
+		
+		mBoard = dao.mainBoardList();
+		mNews = dao.newsSearch();
+		
+		model.addAttribute("mBoard", mBoard);
+		model.addAttribute("mNews", mNews);
 		
 		return "main";
 	}
